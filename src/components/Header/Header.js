94318028ccdb0220845a61bf869e5697371b6faf
Karl Horky <karl.horky@gmail.com>
2016-11-20 18:19:56 +0100

    Start moving soapbox content to Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,28 @@
 /* @flow */
 import React from 'react';
-import logo from './logo.svg';
+import styles from './Header.css';
 
 export default () =>
-  <div className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <h2>Welcome to React</h2>
-  </div>;
+  <section className={styles.className} itemScope itemType="Person">
+    <div className="container">
+      <div className="soapbox-content">
+        <h1 className="soapbox-title" itemProp="name">Karl Horky</h1>
+        <div className="soapbox-subtitle" itemProp="jobTitle">Front-end Engineer</div>
+        <div className="soapbox-lead" itemProp="quote">
+          I design and develop <a href="#work" data-scroll>compelling experiences</a> to improve quality of life through the web.
+        </div>
+        <div className="soapbox-footer">
+          <span className="soapbox-body" itemProp="address" itemScope itemType="Address">
+            <span itemProp="addressLocality">Vienna</span>,
+            <span itemProp="addressCountry">Austria</span>
+          </span>
+          <span className="soapbox-list">
+            <a className="soapbox-body" href="/karl-horky.pdf" download>CV</a>
+            <a className="soapbox-body" href="https://github.com/karlhorky" itemProp="url">GitHub</a>
+            <a className="soapbox-body" href="http://www.linkedin.com/in/karlhorky" itemProp="url">LinkedIn</a>
+            <a className="soapbox-body" href="https://twitter.com/karlhorky" itemProp="url">Twitter</a>
+          </span>
+        </div>
+      </div>
+    </div>
+  </section>;
