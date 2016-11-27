dd4e601274c0ec09137ed9c00021553fcc9fc178
Karl Horky <karl.horky@gmail.com>
2016-11-27 19:25:44 +0100

    Add CSS Modules, style Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,26 @@
 /* @flow */
 import React from 'react';
-import styles from './Header.css';
+import styles from './Header.scss';
 
 export default () =>
-  <section className={styles.className} itemScope itemType="Person">
+  <section className={styles.soapbox} itemScope itemType="Person">
     <div className="container">
-      <div className="soapbox-content">
-        <h1 className="soapbox-title" itemProp="name">Karl Horky</h1>
-        <div className="soapbox-subtitle" itemProp="jobTitle">Front-end Engineer</div>
-        <div className="soapbox-lead" itemProp="quote">
+      <div className={styles.soapboxContent}>
+        <h1 className={styles.soapboxTitle} itemProp="name">Karl Horky</h1>
+        <div className={styles.soapboxSubtitle} itemProp="jobTitle">Front-end Engineer</div>
+        <div className={styles.soapboxLead} itemProp="quote">
           I design and develop <a href="#work" data-scroll>compelling experiences</a> to improve quality of life through the web.
         </div>
-        <div className="soapbox-footer">
-          <span className="soapbox-body" itemProp="address" itemScope itemType="Address">
+        <div className={styles.soapboxFooter}>
+          <span className={styles.soapboxBody} itemProp="address" itemScope itemType="Address">
             <span itemProp="addressLocality">Vienna</span>,
             <span itemProp="addressCountry">Austria</span>
           </span>
-          <span className="soapbox-list">
-            <a className="soapbox-body" href="/karl-horky.pdf" download>CV</a>
-            <a className="soapbox-body" href="https://github.com/karlhorky" itemProp="url">GitHub</a>
-            <a className="soapbox-body" href="http://www.linkedin.com/in/karlhorky" itemProp="url">LinkedIn</a>
-            <a className="soapbox-body" href="https://twitter.com/karlhorky" itemProp="url">Twitter</a>
+          <span className={styles.soapboxList}>
+            <a className={styles.soapboxBody} href="/karl-horky.pdf" download>CV</a>
+            <a className={styles.soapboxBody} href="https://github.com/karlhorky" itemProp="url">GitHub</a>
+            <a className={styles.soapboxBody} href="http://www.linkedin.com/in/karlhorky" itemProp="url">LinkedIn</a>
+            <a className={styles.soapboxBody} href="https://twitter.com/karlhorky" itemProp="url">Twitter</a>
           </span>
         </div>
       </div>
